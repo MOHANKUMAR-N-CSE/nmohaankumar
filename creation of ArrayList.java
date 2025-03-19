3c3723517f5e46e9c42d79e7839f9bb98fbee225
Mohan-kumar <150273070+MOHANKUMAR-N-CSE@users.noreply.github.com>
2025-03-19 20:34:38 +0530

    creation of ArrayList.java

---
+++ creation of ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String>names=new ArrayList<>();
+        names.add("papaya");
+        names.add("banana");
+        names.add("cocunt");
+        System.out.println(names);
+    }
+}
